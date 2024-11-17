54ca9798b15f9e00e002191c2ef9ff6bc81bc8db
GitHub Action <action@github.com>
2024-11-17 11:59:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/franmajka/datagripimportparams/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/franmajka/datagripimportparams/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.franmajka.datagripimportparams.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.franmajka.datagripimportparams.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
